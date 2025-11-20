77a86496df3edd1661f6ee965d08cb4e305f856c
udith444 <foggmaster99@gmail.com>
2025-11-20 18:55:33 +0530

    Modify finder-test.sh for Assignment 4: path-independent operation and result file creation

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Tester script for assignment 1 and assignment 2
 # Author: Siddhant Jajoo
+# Modified for Assignment 4 - Path independent for Buildroot
 
 set -e
 set -u
@@ -8,21 +9,31 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+
+# Modified for Assignment 4: Use absolute path for config files
+if [ -fconf/username.txt ]; then
+    # If running from original location (testing)
+    username=$(cat conf/username.txt)
+    assignment=$(cat conf/assignment.txt)
+else
+    # If running from Buildroot target (files in /etc/finder-app/conf/)
+    username=$(cat /etc/finder-app/conf/username.txt)
+    assignment=$(cat /etc/finder-app/conf/assignment.txt)
+fi
 
 if [ $# -lt 3 ]
 then
-	echo "Using default value ${WRITESTR} for string to write"
-	if [ $# -lt 1 ]
-	then
-		echo "Using default value ${NUMFILES} for number of files to write"
-	else
-		NUMFILES=$1
-	fi	
+    echo "Using default value ${WRITESTR} for string to write"
+    if [ $# -lt 1 ]
+    then
+        echo "Using default value ${NUMFILES} for number of files to write"
+    else
+        NUMFILES=$1
+    fi    
 else
-	NUMFILES=$1
-	WRITESTR=$2
-	WRITEDIR=/tmp/aeld-data/$3
+    NUMFILES=$1
+    WRITESTR=$2
+    WRITEDIR=/tmp/aeld-data/$3
 fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
@@ -32,47 +43,43 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
-
-if [ $assignment != 'assignment1' ]
+if [ "$assignment" != 'assignment1' ]
 then
-	mkdir -p "$WRITEDIR"
+    mkdir -p "$WRITEDIR"
 
-	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
-	#The quotes signify that the entire string in WRITEDIR is a single string.
-	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-	if [ -d "$WRITEDIR" ]
-	then
-		echo "$WRITEDIR created"
-	else
-		exit 1
-	fi
+    #The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
+    #The quotes signify that the entire string in WRITEDIR is a single string.
+    #This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
+    if [ -d "$WRITEDIR" ]
+    then
+        echo "$WRITEDIR created"
+    else
+        exit 1
+    fi
 fi
-#echo "Removing the old writer utility and compiling as a native application"
-#make clean
-#make
 
+# Modified for Assignment 4: Use executables from PATH instead of ./
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+    writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 
-# remove temporary directories
+# Remove temporary directories
 rm -rf /tmp/aeld-data
 
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
-	echo "success"
-	exit 0
+    echo "success"
+    
+    # ADDED FOR ASSIGNMENT 4: Write result file
+    echo "Assignment 4 completed successfully - $(date)" > /tmp/assignment4-result.txt
+    echo "finder-test.sh completed successfully - result written to /tmp/assignment4-result.txt"
+    
+    exit 0
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
-	exit 1
+    echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+    exit 1
 fi
-writer
-finder.sh /tmp/alee/assignment-4-result.txt
-
-# Write assignment 4 result
-echo "Assignment 4 completed successfully" > /tmp/assignment4-result.txt
