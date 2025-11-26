39d45c822133492ca198891e79379e60ce9a0eff
udith444 <foggmaster99@gmail.com>
2025-11-26 19:28:10 +0530

    Update finder-test.sh for Assignment 4 compatibility

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # Tester script for assignment 1 and assignment 2
 # Author: Siddhant Jajoo
-# Modified for Assignment 4
 
 set -e
 set -u
@@ -9,28 +8,22 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-
-# Assignment 4: Use absolute paths for config files
-CONF_DIR="/etc/finder-app/conf"
-username=$(cat "${CONF_DIR}/username.txt")
-assignment=$(cat "${CONF_DIR}/assignment.txt")
-
-# Assignment 4: Don't change directory - assume executables are in PATH
-# cd `dirname $0`
+# username=$(cat conf/username.txt)
+username=$(cat /home/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
-    echo "Using default value ${WRITESTR} for string to write"
-    if [ $# -lt 1 ]
-    then
-        echo "Using default value ${NUMFILES} for number of files to write"
-    else
-        NUMFILES=$1
-    fi    
+	echo "Using default value ${WRITESTR} for string to write"
+	if [ $# -lt 1 ]
+	then
+		echo "Using default value ${NUMFILES} for number of files to write"
+	else
+		NUMFILES=$1
+	fi	
 else
-    NUMFILES=$1
-    WRITESTR=$2
-    WRITEDIR=/tmp/aeld-data/$3
+	NUMFILES=$1
+	WRITESTR=$2
+	WRITEDIR=/tmp/aeld-data/$3
 fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
@@ -40,41 +33,44 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-if [ "$assignment" != 'assignment1' ]
+# assignment=`cat ../conf/assignment.txt`
+assignment=`cat /home/conf/assignment.txt`
+
+if [ $assignment != 'assignment1' ]
 then
-    mkdir -p "$WRITEDIR"
+	mkdir -p "$WRITEDIR"
 
-    if [ -d "$WRITEDIR" ]
-    then
-        echo "$WRITEDIR created"
-    else
-        exit 1
-    fi
+	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
+	#The quotes signify that the entire string in WRITEDIR is a single string.
+	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
+	if [ -d "$WRITEDIR" ]
+	then
+		echo "$WRITEDIR created"
+	else
+		exit 1
+	fi
 fi
+# echo "Removing the old writer utility and compiling as a native application"
+# make clean
+# make
 
-echo "Using writer utility from PATH"
-
-# Assignment 4: Call writer directly (it should be in PATH)
-for i in $(seq 1 $NUMFILES)
+for i in $( seq 1 $NUMFILES)
 do
-    writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-# Assignment 4: Call finder.sh directly (it should be in PATH)
-OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
-
-# Assignment 4: Write result to required location
-echo "$OUTPUTSTRING" > /tmp/assignment4-result.txt
+OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
 set +e
-echo "${OUTPUTSTRING}" | grep "${MATCHSTR}"
+echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
-    echo "success"
-    exit 0
+	echo "success"
+	exit 0
 else
-    echo "failed: expected ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
-    exit 1
+	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+	exit 1
 fi
+
